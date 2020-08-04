64277d22706872520cd26babf88aa471ca37d24d
hieu98 <hieucot1998@gmail.com>
2020-08-05 03:00:15 +0700

    second commit

---
+++ app/src/main/java/com/example/apptracnghiem/Util/NetwordProvider.kt
@@ -0,0 +1,10 @@
+package com.example.apptracnghiem.Util
+
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+
+class NetwordProvider {
+    var mInstance : NetwordProvider? = null
+    private lateinit var retrofit :Retrofit
+
+}
